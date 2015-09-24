22cf6881589e5b00177688c967da8aa02d2dc09e
Lucas McDaniel <ldmcdaniel@gmail.com>
2015-09-24 13:13:33 -0500

    working backend

---
+++ routes/routes.js
@@ -0,0 +1,35 @@
+var User = require('../models/todo');
+
+module.exports = function (app) {
+
+    app.get('/api/todos', function(req, res) {
+      User.find(function(err, todos) {
+        if (err) throw err
+        res.json(todos);
+      });
+    });
+
+    app.post('/api/todos', function(req, res) {
+      User.create({
+        todo: req.body.todo
+      }, function(err, todo) {
+          if (err) throw err
+        User.find(function(err, todos) {
+          if (err) throw err
+          res.json(todos);
+        });
+      });
+    })
+
+    app.delete('/api/todos/:todo_id', function(req, res) {
+      User.remove({
+        _id : req.params.todo_id
+      }, function(err, todo) {
+        if (err) throw err
+        User.find(function(err, todos) {
+          if (err) throw err
+          res.json(todos);
+        });
+      });
+  })
+}
--- server.js
+++ server.js
@@ -1,61 +1,23 @@
-    // set up ========================
-    var express         = require('express'),
-        app             = express(),
-        mongoose        = require('mongoose'),
-        morgan          = require('morgan'),
-        bodyParser      = require('body-parser'),
-        methodOverride  = require('method-override'),
-        database        = require('./lib/mongodb');
+// set up ========================
+var express         = require('express'),
+    app             = express(),
+    morgan          = require('morgan'),
+    mongoose        = require('mongoose'),
+    bodyParser      = require('body-parser'),
+    methodOverride  = require('method-override'),
+    database        = require('./lib/mongodb');
 
-    // configuration =================
+// database configuration ========
 
-    mongoose.connect(database.url);
+app
+  .use(morgan('dev'))
+  .use(bodyParser.urlencoded({'extended':'true'}))
+  .use(bodyParser.json())
+  .use(bodyParser.json({ type: 'application/vnd.api+json' }))
+  .use(methodOverride());
 
-    app
-      .use(morgan('dev'))
-      .use(bodyParser.urlencoded({'extended':'true'}))
-      .use(bodyParser.json())
-      .use(bodyParser.json({ type: 'application/vnd.api+json' }))
-      .use(methodOverride());
+mongoose.connect(database.url);
+require('./routes/routes')(app);
 
-    var User = mongoose.model('User', {
-      username: String,
-      password: String,
-      todo: [{group: String, task: String}]
-    });
-
-    app
-      .get('/api/todos', function(req, res) {
-        User.find(function(err, todos) {
-          if (err) throw err
-          res.json(todos);
-        });
-      })
-
-      .post('/api/todos', function(req, res) {
-        console.log(req.body.todo);
-        User.create({
-          todo: req.body.todo
-        }, function(err, todo) {
-            if (err) throw err
-          User.find(function(err, todos) {
-            if (err) throw err
-            res.json(todos);
-          });
-        });
-      })
-
-      .delete('/api/todos/:todo_id', function(req, res) {
-        User.remove({
-          _id : req.params.todo_id
-        }, function(err, todo) {
-          if (err) throw err
-          User.find(function(err, todos) {
-            if (err) throw err
-            res.json(todos);
-          });
-        });
-      })
-
-      .listen(8080);
+  app.listen(8080);
 
